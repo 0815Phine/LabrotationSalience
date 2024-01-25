0cd082e4776c4f50f037f31624b4d37a49f421e3
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-01-25 13:22:12 +0100

    minor adjustments + paired statistics

--- LearnindSpeed_Slope.m
+++ LearnindSpeed_Slope.m
@@ -1,47 +1,52 @@
-clearvars
-close all
+% clearvars
+% close all
+% 
+% startPath = 'Z:\Filippo\Animals';
+% try
+%     load(fullfile(startPath,'animalData.mat'))
+% catch
+%     fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
+%     fprintf(2,'\nYou have to create it first.\n\n')
+%     return
+% end
 
-startPath = 'Z:\Filippo\Animals';
-try
-    load(fullfile(startPath,'animalData.mat'))
-catch
-    fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
-    fprintf(2,'\nYou have to create it first.\n\n')
-    return
+%% create slope arrays
+% ATTENTION: to run this script animalData.mat has do be loaded into the workspace
+% load cohort-data for 20mm contrast
+cohorts = [11, 14, 17];
+FieldofChoice = {'slope_initial', 'slope_second'};
+
+Slope20_ini = [];
+Slope20_swi = horzcat(animalData.cohort(11).animal.(FieldofChoice{2}));
+for cohortIDX = 1:length(cohorts)
+    cohort_Data = animalData.cohort(cohorts(cohortIDX)).animal;
+    Slope20_ini = horzcat(Slope20_ini, cohort_Data.(FieldofChoice{1}));
 end
-
-% load Cohort Data for 20mm contrast
-cohort_Data11 = animalData.cohort(11).animal;
-cohort_Data14 = animalData.cohort(14).animal;
-cohort_Data17 = animalData.cohort(17).animal;
-
-%% create slope variables
-Slope20_ini = horzcat(cohort_Data11.slope_initial,cohort_Data14.slope_initial,cohort_Data17.slope_initial);
-Slope20_sec = horzcat(cohort_Data11.slope_second);
 
 %Slope20_ini_std = std(Slope20_ini,0,2,'omitnan');
 Slope20_ini_mean = mean(Slope20_ini,2,'omitnan');
 
 %Slope20_sec_std = std(Slope20_sec,0,2,'omitnan');
-Slope20_sec_mean = mean(Slope20_sec,2,'omitnan');
+Slope20_swi_mean = mean(Slope20_swi,2,'omitnan');
 
-[p0,~] = ranksum(Slope20_ini,Slope20_sec);
+%[p,~] = ranksum(Slope20_ini,Slope20_swi);
+p_paired = signrank(Slope20_ini(1,1:length(Slope20_swi)), Slope20_swi);
 
 %% line plot
 figure, hold on
 
 xvalues = ones(1,length(Slope20_ini)); scatter(xvalues,Slope20_ini, 'k','filled')
-xvalues = ones(1,length(Slope20_sec)); scatter(xvalues+1,Slope20_sec, 'k','filled'),
+xvalues = ones(1,length(Slope20_swi)); scatter(xvalues+1,Slope20_swi, 'k','filled'),
 
-for i = 1:length(Slope20_sec)
-    plot([1,2],[Slope20_ini(i),Slope20_sec(i)],'Color','k')
+for i = 1:length(Slope20_swi)
+    plot([1,2],[Slope20_ini(i),Slope20_swi(i)],'Color','k')
 end
 
-plot([1,2],[Slope20_ini_mean, Slope20_sec_mean],'LineWidth', 1.5)
+plot([1,2],[Slope20_ini_mean, Slope20_swi_mean],'LineWidth', 1.5)
 
-slope_all = horzcat(Slope20_ini,Slope20_sec);
+slope_all = horzcat(Slope20_ini,Slope20_swi);
 slope_max = max(slope_all);
-plotStatistics(p0,slope_max,1,2)
+plotStatistics(p_paired,slope_max,1,2)
 
 title('learning speed per animal')
 xticks([1,2]), xticklabels({'initial rule','switched rule'})

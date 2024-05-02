ecf8c81f825853a211e938dde40adb60ce21df88
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-05-02 13:31:17 +0200

    use plotStatistics function

--- whiskerpluck.m
+++ whiskerpluck.m
@@ -1,14 +1,14 @@
-clearvars
-close all
-
-startPath = 'Z:\Filippo\Animals';
-try
-    load(fullfile(startPath,'animalData.mat'))
-catch
-    fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
-    fprintf(2,'\nYou have to create it first.\n\n')
-    return
-end
+% clearvars
+% close all
+% 
+% startPath = 'Z:\Filippo\Animals';
+% try
+%     load(fullfile(startPath,'animalData.mat'))
+% catch
+%     fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
+%     fprintf(2,'\nYou have to create it first.\n\n')
+%     return
+% end
 
 %% create whiskerpluck table
 animalSelection = [11 14 16 17];
@@ -54,6 +54,7 @@ p = nan(2, 6);
 
 p(1,1) = anova1(whiskerpluck_table(:,3),whiskerpluck_table(:,4),'off');
 p(2,1) = ranksum(whiskerpluck_table(whiskerpluck_table(:,4)==1,3),whiskerpluck_table(whiskerpluck_table(:,4)==2,3));
+
 if p(1,1) < 0.05 && p(2,1) < 0.05
     fprintf('The d prime is significantly different before and after the whiskerpluck, p =  %.3f\n', p(2,1));
 end
@@ -82,22 +83,9 @@ yline([1.65, 1.65],'Color','black','LineStyle','--')
 yline([0, 0],'Color',[.7 .7 .7],'LineStyle','--')
 xlim([0 3])
 ylim([-0.5 4.5])
-
 dprime_max = arrayfun(@(c) max(whiskerpluck_table(whiskerpluck_table(:,1) == c, 3)), contrast);
-
-if p(2,1) < 0.05
-   plot([1 2],[max(dprime_max)+0.2 max(dprime_max)+0.2], 'k')
-end
-
-if p(2,1) <= 0.05 && p(2,1) > 0.01
-   text(1.5, max(dprime_max)+0.3,'*','HorizontalAlignment','center')
-elseif p(2,1) <= 0.01 && p(2,1) > 0.001
-    text(1.5, max(dprime_max)+0.3,'**','HorizontalAlignment','center')
-elseif p(2,1) <= 0.001
-    text(1.5, max(dprime_max)+0.3,'***','HorizontalAlignment','center')
-end
-
-% savefig(f1, fullfile('Z:\Josephine\Master-Thesis_Figures\Whiskerpluck','whiskerpluck_all.fig'))
+plotStatistics(p(2,1),max(dprime_max),1,2)
+%savefig(f1, fullfile('Z:\Josephine\Master-Thesis_Figures\Whiskerpluck','whiskerpluck_all.fig'))
 
 % all whiskerplucks combined before and after (1 session)
 sesFlag = (whiskerpluck_table(1:120,2)<2);
@@ -109,25 +97,18 @@ yline([1.65, 1.65],'Color','black','LineStyle','--')
 yline([0, 0],'Color',[.7 .7 .7],'LineStyle','--')
 xlim([0 3])
 ylim([-0.5 4.5])
+plotStatistics(p(2,1),max(dprime_max),1,2)
+%savefig(f2, fullfile('Z:\Josephine\Master-Thesis_Figures\Whiskerpluck','whiskerpluck_all_first.fig'))
 
-if p(2,1) < 0.05
-   plot([1 2],[max(dprime_max)+0.2 max(dprime_max)+0.2], 'k')
-end
-
-if p(2,1) <= 0.05 && p(2,1) > 0.01
-   text(1.5, max(dprime_max)+0.3,'*','HorizontalAlignment','center')
-elseif p(2,1) <= 0.01 && p(2,1) > 0.001
-    text(1.5, max(dprime_max)+0.3,'**','HorizontalAlignment','center')
-elseif p(2,1) <= 0.001
-    text(1.5, max(dprime_max)+0.3,'***','HorizontalAlignment','center')
-end
-
-% savefig(f2, fullfile('Z:\Josephine\Master-Thesis_Figures\Whiskerpluck','whiskerpluck_all_first.fig'))
-
-% for individual contrast
+% for individual contrast first before and first after
 for i= contrast
-    flag = (whiskerpluck_table(1:120,1) == i)+(whiskerpluck_table(1:120,2)<2);
-    loopf = figure; boxchart(whiskerpluck_table(flag == 2,4), whiskerpluck_table(flag == 2,3),'BoxFaceColor', 'k','MarkerColor','k')
+    contFlag = (whiskerpluck_table(1:120,1) == i);
+    sesFlagbefore = (whiskerpluck_table(1:120,2)==-1);
+    sesFlagafter = (whiskerpluck_table(1:120,2)==1);
+    plotflag = contFlag+sesFlagbefore+sesFlagafter;
+    loopf = figure; hold on
+    boxchart(whiskerpluck_table(plotflag == 2,4), whiskerpluck_table(plotflag == 2,3),'BoxFaceColor', 'k','MarkerColor','k')
+    scatter(whiskerpluck_table(plotflag == 2,4), whiskerpluck_table(plotflag == 2,3),'Marker','.','Jitter','on','MarkeredgeColor','k')
     hold on; xticks([1,2]); xticklabels({'pre whiskerpluck','post whiskerpluck'})
     ylabel('d prime')
     title(sprintf('Performance for contrast: %dmm',i))
@@ -137,18 +118,7 @@ for i= contrast
 
     ii = [false, ismember(contrast, i)];
     iii = [ismember(contrast, i)];
-    if p(2,ii) < 0.05
-        plot([1 2],[dprime_max(iii)+0.2 dprime_max(iii)+0.2], 'k')
-    end
-    
-    
-    if p(2,ii) <= 0.05 && p(2,ii) > 0.01
-        text(1.5, dprime_max(iii)+0.3,'*','HorizontalAlignment','center')
-    elseif p(2,ii) <= 0.01 && p(2,ii) > 0.001
-        text(1.5, dprime_max(iii)+0.3,'**','HorizontalAlignment','center')
-    elseif p(2,ii) <= 0.001
-        text(1.5, dprime_max(iii)+0.3,'***','HorizontalAlignment','center')
-    end
+    %plotStatistics(p(2,ii),dprime_max(iii),1,2)
 
     % savefig(loopf, fullfile('Z:\Josephine\Master-Thesis_Figures\Whiskerpluck',sprintf('whiskerpluck_%d.fig',i)))
 end

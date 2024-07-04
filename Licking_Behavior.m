e592c9b943a67dad953518ac1be2ad7cc3561122
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-07-04 13:30:25 +0200

    prompt to choose cohort

--- Licking_Behavior.m
+++ Licking_Behavior.m
@@ -1,145 +1,165 @@
-clearvars
-close all
+% clearvars
+% close all
+% 
+% startPath = 'Z:\Filippo\Animals';
+% try
+%     load(fullfile(startPath,'animalData.mat'))
+% catch
+%     fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
+%     fprintf(2,'\nYou have to create it first.\n\n')
+%     return
+% end
 
-startPath = 'Z:\Filippo\Animals';
-try
-    load(fullfile(startPath,'animalData.mat'))
-catch
-    fprintf(2,'\nThe variable "animalData.mat" doesn''t exist.')
-    fprintf(2,'\nYou have to create it first.\n\n')
-    return
-end
+%% choose cohorts
+cohorts = arrayfun(@(x) num2str(x), 1:numel(animalData.cohort), 'UniformOutput', false);
+answer = listdlg('ListString',cohorts,'PromptString','Choose your cohort.');
+cohorts = cellfun(@str2double, cohorts(answer));
+cohortData = horzcat(animalData.cohort(cohorts).animal);
 
 %% get lick rates
-allgosuc_initial = NaN(47,19);
-allnogosuc_initial = NaN(47,19);
-allgosuc_switched = NaN(54,6);
-allnogosuc_switched = NaN(54,6);
+numMice = length(cohortData);
+max_gosuc = max(arrayfun(@(m) length(cohortData(m).gogo_suc), 1:sum(numMice)));
+max_nogosuc = max(arrayfun(@(m) length(cohortData(m).nogo_suc), 1:sum(numMice)));
+allgosuc_initial = NaN(max_gosuc, numMice);
+allnogosuc_initial = NaN(max_nogosuc, numMice);
+allgosuc_switched = NaN(max_gosuc, numMice);
+allnogosuc_switched = NaN(max_nogosuc, numMice);
 
-cohortFlag = [11 14 17];
 all_ses_ini = []; all_ses_swi = all_ses_ini;
-for ii = cohortFlag
-    cohortData = animalData.cohort(ii).animal;
-    for i = 1:length(cohortData)
-        isP2 = contains(cohortData(i).session_names,'P3.2');
-        sesFlag_first = find(isP2, 1, 'first');
-        sesFlag_last = find(isP2, 1, 'last');
-        num_ses = sesFlag_last-sesFlag_first;
+for i = 1:length(cohortData)
+    % initial rule
+    isP2 = contains(cohortData(i).session_names,'P3.2');
+    sesFlag_first = find(isP2, 1, 'first');
+    sesFlag_last = find(isP2, 1, 'last');
+    num_ses = sesFlag_last-sesFlag_first;
 
-        norm_ses_ini = (1:num_ses)/num_ses;
-        all_ses_ini = cat(1, all_ses_ini(:), {norm_ses_ini});
+    norm_ses_ini = (1:num_ses)/num_ses;
+    all_ses_ini = cat(1, all_ses_ini(:), {norm_ses_ini});
 
-        gosuc = cohortData(i).gogo_suc;
-        gosuc(sesFlag_last+1:end) = [];
-        gosuc(1:sesFlag_first-1) = [];
-        if ii == 11
-            allgosuc_initial(1:length(gosuc),i) = gosuc;
-        elseif ii == 14
-            allgosuc_initial(1:length(gosuc),i+6) = gosuc;
-        elseif ii == 17
-            allgosuc_initial(1:length(gosuc),i+14) = gosuc;
-        end
+    gosuc = cohortData(i).gogo_suc;
+    gosuc(sesFlag_last+1:end) = [];
+    gosuc(1:sesFlag_first-1) = [];
+    allgosuc_initial(1:length(gosuc),i) = gosuc;
 
-        nogosuc = cohortData(i).nogo_suc;
-        nogosuc(sesFlag_last+1:end) = [];
-        nogosuc(1:sesFlag_first-1) = [];
-        if ii == 11
-            allnogosuc_initial(1:length(nogosuc),i) = nogosuc;
-        elseif ii == 14
-            allnogosuc_initial(1:length(nogosuc),i+6) = nogosuc;
-        elseif ii == 17
-            allnogosuc_initial(1:length(nogosuc),i+14) = nogosuc;
-        end
+    nogosuc = cohortData(i).nogo_suc;
+    nogosuc(sesFlag_last+1:end) = [];
+    nogosuc(1:sesFlag_first-1) = [];
+    allnogosuc_initial(1:length(nogosuc),i) = nogosuc;
 
-        if ii==17 || ii==14
-            continue
-        else
-            isP4 = contains(cohortData(i).session_names,'P3.4');
-            sesFlag_first = find(isP4, 1, 'first');
-            sesFlag_last = find(isP4, 1, 'last');
-            num_ses = sesFlag_last-sesFlag_first;
+    % reversed rule
+    isP4 = contains(cohortData(i).session_names,'P3.4');
+    sesFlag_first = find(isP4, 1, 'first');
+    sesFlag_last = find(isP4, 1, 'last');
+    num_ses = sesFlag_last-sesFlag_first;
 
-            norm_ses_swi = (1:num_ses)/num_ses;
-            all_ses_swi = cat(1, all_ses_swi(:), {norm_ses_swi});
+    norm_ses_swi = (1:num_ses)/num_ses;
+    all_ses_swi = cat(1, all_ses_swi(:), {norm_ses_swi});
 
-            gosuc = cohortData(i).gogo_suc;
-            gosuc(sesFlag_last+1:end) = [];
-            gosuc(1:sesFlag_first-1) = [];
-            allgosuc_switched(1:length(gosuc),i) = gosuc;
+    gosuc = cohortData(i).gogo_suc;
+    gosuc(sesFlag_last+1:end) = [];
+    gosuc(1:sesFlag_first-1) = [];
+    allgosuc_switched(1:length(gosuc),i) = gosuc;
 
-            nogosuc = cohortData(i).nogo_suc;
-            nogosuc(sesFlag_last+1:end) = [];
-            nogosuc(1:sesFlag_first-1) = [];
-            allnogosuc_switched(1:length(nogosuc),i) = nogosuc;
-        end
-    end
+    nogosuc = cohortData(i).nogo_suc;
+    nogosuc(sesFlag_last+1:end) = [];
+    nogosuc(1:sesFlag_first-1) = [];
+    allnogosuc_switched(1:length(nogosuc),i) = nogosuc;
 end
-allnogosuc_initial(:,17) = [];
-allgosuc_initial(:,17) = [];
-all_ses_ini(17) = [];
-
-% figure; hold on; arrayfun(@(x) plot(all_ses_ini{x}, allnogosuc_initial(1:numel(all_ses_ini{x}),x), 'LineWidth', 1/3, 'Color', 'k'), 1:numel(all_ses_ini))
-
-%{
-figure, hold on
-allgosuc_initial_mean = mean(allgosuc_initial,2,'omitnan'); allgosuc_initial_mean(isnan(allgosuc_initial_mean)) =[];
-allgosuc_initial_std = std(allgosuc_initial,0,2,'omitnan'); allgosuc_initial_std(isnan(allgosuc_initial_std)) =[];
-curve1 = allgosuc_initial_mean + allgosuc_initial_std;
-curve2 = allgosuc_initial_mean - allgosuc_initial_std;
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','#77AC30', 'EdgeColor','none','FaceAlpha',0.25); hold on
-plot(1:length(allgosuc_initial_mean),allgosuc_initial_mean, 'Color', '#77AC30')
-allnogosuc_initial_mean = mean(allnogosuc_initial,2,'omitnan'); allnogosuc_initial_mean(isnan(allnogosuc_initial_mean)) =[];
-allnogosuc_initial_std = std(allnogosuc_initial,0,2,'omitnan'); allnogosuc_initial_std(isnan(allnogosuc_initial_std)) =[];
-curve1 = 1-allnogosuc_initial_mean + allnogosuc_initial_std;
-curve2 = 1-allnogosuc_initial_mean - allnogosuc_initial_std;
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','#A2142F', 'EdgeColor','none','FaceAlpha',0.25); hold on
-plot(1:length(allnogosuc_initial_mean),(1-allnogosuc_initial_mean), 'Color', '#A2142F')
-xlabel('Sessions')
-ylabel('Lick rate')
-title ('Population lick rates (initial rule; contrast 20mm)')
-ylim([0 1]); xlim([1 46])
-%}
-%{
-figure, hold on
-allgosuc_switched_mean = mean(allgosuc_switched,2,'omitnan'); allgosuc_switched_mean(isnan(allgosuc_switched_mean)) =[];
-allgosuc_switched_std = std(allgosuc_switched,0,2,'omitnan'); allgosuc_switched_std(isnan(allgosuc_switched_std)) =[];
-curve1 = allgosuc_switched_mean + allgosuc_switched_std;
-curve2 = allgosuc_switched_mean - allgosuc_switched_std;
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','#77AC30', 'EdgeColor','none','FaceAlpha',0.25); hold on
-plot(1:length(allgosuc_switched_mean),allgosuc_switched_mean, 'Color', '#77AC30')
-allnogosuc_switched_mean = mean(allnogosuc_switched,2,'omitnan'); allnogosuc_switched_mean(isnan(allnogosuc_switched_mean)) =[];
-allnogosuc_switched_std = std(allnogosuc_switched,0,2,'omitnan'); allnogosuc_switched_std(isnan(allnogosuc_switched_std)) =[];
-curve1 = 1-allnogosuc_switched_mean + allnogosuc_switched_std;
-curve2 = 1-allnogosuc_switched_mean - allnogosuc_switched_std;
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','#A2142F', 'EdgeColor','none','FaceAlpha',0.25); hold on
-plot(1:length(allnogosuc_switched_mean),(1-allnogosuc_switched_mean), 'Color', '#A2142F')
-xlabel('Sessions')
-ylabel('Lick rate')
-title ('Population lick rates (switched rule; contrast 20mm)')
-ylim([0 1]); xlim([1 53])
-%}
 
 %% plot data
 fig_1 = plot_patch(1-allnogosuc_initial,all_ses_ini,'r',30);
 plot_patch(allgosuc_initial,all_ses_ini,'g',30,fig_1);
-title('Population lick rates (initial rule; contrast 20mm)')
+title('Population lick rates (initial rule)')
 xlabel('Session proportion'); ylabel('Lick rate')
-legend({'No-Go failure' '' 'Go success'}, 'Box', 'off', 'Location', 'best')
+legend({'No-go trials' '' 'Go trials'}, 'Box', 'off', 'Location', 'best')
 set(gca,'Box','off','Color','none')
 
 fig_2 = plot_patch(1-allnogosuc_switched,all_ses_swi,'r',40);
 plot_patch(allgosuc_switched,all_ses_swi,'g',40,fig_2);
-title('Population lick rates (switched rule; contrast 20mm)')
+title('Population lick rates (switched rule)')
 xlabel('Session proportion'); ylabel('Lick rate')
-legend({'No-Go failure' '' 'Go success'}, 'Box', 'off', 'Location', 'best')
+legend({'No-go trials' '' 'Go trials'}, 'Box', 'off', 'Location', 'best')
 set(gca,'Box','off','Color','none')
 
-saveFigure(fig_1,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Initial_20'),true,true)
-saveFigure(fig_2,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Switched_20'),true,true)
+%saveFigure(fig_1,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Initial_20'),true,true)
+%saveFigure(fig_2,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Switched_20'),true,true)
+
+%% neutral state
+all_ses_neu = [];
+for i = 1:length(cohortData)
+    isP3 = contains(cohortData(i).session_names,'P3.3');
+    sesFlag_first = find(isP3, 1, 'first');
+    sesFlag_last = find(isP3, 1, 'last');
+    num_ses = sesFlag_last-sesFlag_first;
+
+    if isempty(num_ses)
+        continue
+    else
+        % norm_ses_neu = (1:num_ses)/num_ses;
+        % all_ses_neu = cat(1, all_ses_neu(:), {norm_ses_neu});
+
+        gosuc = cohortData(i).gogo_suc;
+        gosuc(sesFlag_last+1:end) = [];
+        gosuc(1:sesFlag_first-1) = [];
+        allgosuc(1:length(gosuc),i) = gosuc;
+
+        nogosuc = cohortData(i).nogo_suc;
+        nogosuc(sesFlag_last+1:end) = [];
+        nogosuc(1:sesFlag_first-1) = [];
+        allnogosuc(1:length(nogosuc),i) = nogosuc;
+
+        neulick = cohortData(i).medium_lick;
+        neulick(sesFlag_last+1:end) = [];
+        neulick(1:sesFlag_first-1) = [];
+        allneutral(1:length(neulick),i) = neulick;
+    end
+end
+
+% plot data with session proportion
+% fig_prop = plot_patch(1-allnogosuc,all_ses_neu,'r',7);
+% plot_patch(allgosuc,all_ses_neu,'g',7,fig_prop);
+% plot_patch(allneutral,all_ses_neu,'#EDB120',7,fig_prop)
+
+% as all animals had 8 sessions in stage 3 we don't necessarly need the
+% proportion function
+xvalues = 1:8;
+
+% nogo trials
+sig_plot = std(1-allnogosuc,1,2,'omitnan');
+mu_plot = mean(1-allnogosuc,2,"omitnan");
+curve1 = mu_plot + sig_plot;
+curve2 = mu_plot - sig_plot;
+
+figure, plot(xvalues, mu_plot, 'Color', 'r'); hold on
+fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
+     'FaceColor','r', 'EdgeColor','none','FaceAlpha',0.1);
+
+% go trials
+sig_plot = std(allgosuc,1,2,'omitnan');
+mu_plot = mean(allgosuc,2,"omitnan");
+curve1 = mu_plot + sig_plot;
+curve2 = mu_plot - sig_plot;
+
+plot(xvalues, mu_plot, 'Color', 'g')
+fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
+     'FaceColor','g', 'EdgeColor','none','FaceAlpha',0.1);
+
+% neutral trials
+sig_plot = std(allneutral,1,2,'omitnan');
+mu_plot = mean(allneutral,2,"omitnan");
+curve1 = mu_plot + sig_plot;
+curve2 = mu_plot - sig_plot;
+
+plot(xvalues, mu_plot, 'Color', '#EDB120')
+fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
+     'FaceColor','#EDB120', 'EdgeColor','none','FaceAlpha',0.1);
+
+% labels
+ylim([0,1])
+title('Population lick rates (neutral aperture state)')
+xlabel('Session'); ylabel('Lick rate')
+legend({'No-go trials' '' 'Go trials' '' 'Neutral trials'}, 'Box', 'off', 'Location', 'best')
+set(gca,'Box','off','Color','none')
 
 %% slow- vs. fast-learner initial rule
 %{
@@ -309,112 +329,5 @@ plot(ax, min(speed20{1}):10:max(speed20{1}), feval(mdls2{1}, min(speed20{1}):10:
 ax = get(fig_4, 'Children');
 plot(ax, xlim(ax), feval(mdls{2}, xlim(ax)), '--k')
 
-saveFigure(fig_3,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Initial_total_events_20'),true, true)
-saveFigure(fig_4,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Switched_total_events_20'),true, true)
-
-%% neutral state
-cohortFlag = [11 12 15 16];
-all_ses_neu = [];
-for ii = cohortFlag
-    cohortData = animalData.cohort(ii).animal;
-    for i = 1:length(cohortData)
-        isP3 = contains(cohortData(i).session_names,'P3.3');
-        sesFlag_first = find(isP3, 1, 'first');
-        sesFlag_last = find(isP3, 1, 'last');
-        num_ses = sesFlag_last-sesFlag_first;
-        
-        if isempty(num_ses)
-            continue
-        else
-            % norm_ses_neu = (1:num_ses)/num_ses;
-            % all_ses_neu = cat(1, all_ses_neu(:), {norm_ses_neu});
-     
-            gosuc = cohortData(i).gogo_suc;
-            gosuc(sesFlag_last+1:end) = [];
-            gosuc(1:sesFlag_first-1) = [];
-            if ii == 11
-                allgosuc(1:length(gosuc),i) = gosuc;
-            elseif ii == 12
-                allgosuc(1:length(gosuc),i+6) = gosuc;
-            elseif ii == 15
-                allgosuc(1:length(gosuc),i+12) = gosuc;
-            elseif ii==16
-                allgosuc(1:length(gosuc),i+13) = gosuc;
-            end
-
-            nogosuc = cohortData(i).nogo_suc;
-            nogosuc(sesFlag_last+1:end) = [];
-            nogosuc(1:sesFlag_first-1) = [];
-            if ii == 11
-                allnogosuc(1:length(nogosuc),i) = nogosuc;
-            elseif ii == 12
-                allnogosuc(1:length(nogosuc),i+6) = nogosuc;
-            elseif ii == 15
-                allnogosuc(1:length(nogosuc),i+12) = nogosuc;
-            elseif ii == 16
-                allnogosuc(1:length(nogosuc),i+13) = nogosuc;
-            end
-
-            neulick = cohortData(i).medium_lick;
-            neulick(sesFlag_last+1:end) = [];
-            neulick(1:sesFlag_first-1) = [];
-            if ii == 11
-                allneutral(1:length(neulick),i) = neulick;
-            elseif ii == 12
-                allneutral(1:length(neulick),i+6) = neulick;
-            elseif ii == 15
-                allneutral(1:length(neulick),i+12) = neulick;
-            elseif ii == 16
-                allneutral(1:length(neulick),i+13) = neulick;
-            end
-        end
-    end
-end
-
-% plot data
-
-% plot data with session proportion
-% fig_prop = plot_patch(1-allnogosuc,all_ses_neu,'r',7);
-% plot_patch(allgosuc,all_ses_neu,'g',7,fig_prop);
-% plot_patch(allneutral,all_ses_neu,'#EDB120',7,fig_prop)
-
-% as all animals had 8 sessions in stage 3 we don't necessarly need the
-% proportion function
-xvalues = 1:8;
-
-%nogo trials
-sig_plot = std(1-allnogosuc,1,2,'omitnan');
-mu_plot = mean(1-allnogosuc,2,"omitnan");
-curve1 = mu_plot + sig_plot;
-curve2 = mu_plot - sig_plot;
-
-fig_5 = plot(xvalues, mu_plot, 'Color', 'r'); hold on
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','r', 'EdgeColor','none','FaceAlpha',0.1);
-
-%go trials
-sig_plot = std(allgosuc,1,2,'omitnan');
-mu_plot = mean(allgosuc,2,"omitnan");
-curve1 = mu_plot + sig_plot;
-curve2 = mu_plot - sig_plot;
-
-plot(xvalues, mu_plot, 'Color', 'g')
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','g', 'EdgeColor','none','FaceAlpha',0.1);
-
-%neutral trials
-sig_plot = std(allneutral,1,2,'omitnan');
-mu_plot = mean(allneutral,2,"omitnan");
-curve1 = mu_plot + sig_plot;
-curve2 = mu_plot - sig_plot;
-
-plot(xvalues, mu_plot, 'Color', '#EDB120')
-fill([1:length(curve1) fliplr(1:length(curve1))], [curve1' fliplr(curve2')],[0 0 .85],...
-     'FaceColor','#EDB120', 'EdgeColor','none','FaceAlpha',0.1);
-
-% labels
-ylim([0,1])
-title('Population lick rates (neutral state; contrast 20mm)')
-xlabel('Session'); ylabel('Lick rate')
-legend({'No-Go trials' '' 'Go trials' '' 'Neutral trials'}, 'Box', 'off', 'Location', 'best')
-set(gca,'Box','off','Color','none')
+%saveFigure(fig_3,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Initial_total_events_20'),true, true)
+%saveFigure(fig_4,fullfile('Z:\Josephine\Master-Thesis_Figures\Lick_Rates','Switched_total_events_20'),true, true)

7ef7d0eac9d2110dab3c322e7929b1b78c3a4f62
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-10-09 15:49:42 +0200

    add plotting on common axes

--- Population_Success_Rate.m
+++ Population_Success_Rate.m
@@ -16,10 +16,16 @@ stages = stages(answer);
 
 %% plotting
 close all; fig_initial = figure; fig_reversed = figure; fig_overall = figure;
+fig_ini_n = figure; fig_rev_n = figure; fig_ove_n = figure;
 numMice = arrayfun(@(x) length(animalData.cohort(x).animal), cohorts);
 max_sessions = max(arrayfun(@(m) length(cohortData(m).gogo_suc), 1:sum(numMice)));
 color_map = [[0 1 0]; [1 0 0]];
 stage_color_map = [[0.1294 0.4 0.6745]; [0.9373 0.5412 0.3843]];
+
+% Create cell arrays to store axes for interpolation later
+axis_go = cell(1, sum(numMice));
+axis_nogo = cell(1, sum(numMice));
+axis_overall = cell(1, sum(numMice));
 
 allgo_suc = NaN(max_sessions,sum(numMice));
 allnogo_suc = NaN(max_sessions,sum(numMice));
@@ -37,16 +43,19 @@ for stageIDX = 1:length(stages)
         % success rate go
         go_suc = cohortData(mouseIDX).gogo_suc(sesFlag_first:sesFlag_last);
         allgo_suc(1:length(go_suc),mouseIDX) = go_suc;
+        axis_go{mouseIDX} = linspace(0, 1, length(go_suc));
         clear go_suc
 
         % success rate nogo
         nogo_suc = cohortData(mouseIDX).nogo_suc(sesFlag_first:sesFlag_last);
         allnogo_suc(1:length(nogo_suc),mouseIDX) = nogo_suc;
+        axis_nogo{mouseIDX} = linspace(0, 1, length(nogo_suc));
         clear nogo_suc
 
         % overall success
         overall_suc = cohortData(mouseIDX).overall_suc(sesFlag_first:sesFlag_last);
         alloverall_suc(1:length(overall_suc),mouseIDX) = overall_suc;
+        axis_overall{mouseIDX} = linspace(0, 1, length(overall_suc));
         clear overall_suc
     end
 
@@ -60,11 +69,23 @@ for stageIDX = 1:length(stages)
         curve1 = sucrate_mean + sucrate_std;
         curve2 = sucrate_mean - sucrate_std;
 
+        % select correct figure
         if strcmp(xfield{fieldIDX}, 'overall')
+            plot_patch(alloverall_suc, axis_overall, stage_color_map(stageIDX,:), 30, fig_ove_n);
             figure(fieldIDX)
         elseif strcmp(stages{stageIDX}, 'P3.2')
+            if strcmp(xfield{fieldIDX}, 'go')
+                plot_patch(allgo_suc, axis_go, color_map(fieldIDX,:), 30, fig_ini_n);
+            elseif strcmp(xfield{fieldIDX}, 'nogo')
+                plot_patch(allnogo_suc, axis_nogo, color_map(fieldIDX,:), 30, fig_ini_n);
+            end
             figure(stageIDX)
         elseif strcmp(stages{stageIDX}, 'P3.4')
+            if strcmp(xfield{fieldIDX}, 'go')
+                plot_patch(allgo_suc, axis_go, color_map(fieldIDX,:), 100, fig_rev_n);
+            elseif strcmp(xfield{fieldIDX}, 'nogo')
+                plot_patch(allnogo_suc, axis_nogo, color_map(fieldIDX,:), 100, fig_rev_n);
+            end
             figure(stageIDX)
         else
             continue

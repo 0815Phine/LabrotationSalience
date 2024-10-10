b67aae9d102505f204f67a6b092210ffd9791256
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2024-10-10 15:24:16 +0200

    add moment of insight

--- Population_Success_Rate.m
+++ Population_Success_Rate.m
@@ -32,7 +32,8 @@ allnogo_suc = NaN(max_sessions,sum(numMice));
 alloverall_suc = NaN(max_sessions,sum(numMice));
 for stageIDX = 1:length(stages)
     for mouseIDX = 1:length(cohortData)
-
+        
+        % find stage and check if animal had training
         isStage = contains(cohortData(mouseIDX).session_names, stages(stageIDX));
         sesFlag_first = find(isStage, 1, 'first');
         sesFlag_last = find(isStage, 1, 'last');
@@ -50,6 +51,16 @@ for stageIDX = 1:length(stages)
         nogo_suc = cohortData(mouseIDX).nogo_suc(sesFlag_first:sesFlag_last);
         allnogo_suc(1:length(nogo_suc),mouseIDX) = nogo_suc;
         axis_nogo{mouseIDX} = linspace(0, 1, length(nogo_suc));
+
+        %find where no-go success rat eincreases by a factor of 5
+        insight_idx = NaN;
+        for i = 2:length(nogo_suc)
+            if nogo_suc(i) >= 5*nogo_suc(i-1) && nogo_suc(i-1) ~= 0
+                insight_idx = i;
+                insight(mouseIDX) = axis_nogo{mouseIDX}(insight_idx);
+                break;
+            end
+        end
         clear nogo_suc
 
         % overall success
@@ -66,48 +77,70 @@ for stageIDX = 1:length(stages)
     for fieldIDX = 1:length(xfield)
         sucrate_mean = mean(sucrate.(xfield{fieldIDX}),2,'omitnan');  sucrate_mean(isnan(sucrate_mean)) =[];
         sucrate_std = std(sucrate.(xfield{fieldIDX}),0,2,'omitnan'); sucrate_std(isnan(sucrate_std)) =[];
-        curve1 = sucrate_mean + sucrate_std;
-        curve2 = sucrate_mean - sucrate_std;
 
-        % select correct figure
+        % plot both overall success rates together
         if strcmp(xfield{fieldIDX}, 'overall')
             plot_patch(alloverall_suc, axis_overall, stage_color_map(stageIDX,:), 30, fig_ove_n);
-            figure(fieldIDX)
-        elseif strcmp(stages{stageIDX}, 'P3.2')
+            xlabel('Training stage progression'); ylabel('Success rate')
+        end
+
+        % select correct figure for stage plotting
+        if strcmp(stages{stageIDX}, 'P3.2')
             if strcmp(xfield{fieldIDX}, 'go')
                 plot_patch(allgo_suc, axis_go, color_map(fieldIDX,:), 30, fig_ini_n);
+                title (sprintf('Population success rates, %s', stages{stageIDX}))
+                xlabel('Training stage progression'); ylabel('Success rate'); ylim([0 1])
             elseif strcmp(xfield{fieldIDX}, 'nogo')
                 plot_patch(allnogo_suc, axis_nogo, color_map(fieldIDX,:), 30, fig_ini_n);
+                xline(mean(nonzeros(insight)), '--k', 'LineWidth', 1.5)
+                text(mean(nonzeros(insight))+0.02,0.05,'Moment of insight','Rotation',90)
+                delete insight
+            elseif strcmp(xfield{fieldIDX}, 'overall')
+                plot_patch(alloverall_suc, axis_overall, stage_color_map(stageIDX,:), 30, fig_ini_n);
+                legend('Go trials','','No-Go trials','','','Overall','Location','southeast','Box','off')
             end
-            figure(stageIDX)
+
         elseif strcmp(stages{stageIDX}, 'P3.4')
             if strcmp(xfield{fieldIDX}, 'go')
-                plot_patch(allgo_suc, axis_go, color_map(fieldIDX,:), 100, fig_rev_n);
+                plot_patch(allgo_suc, axis_go, color_map(fieldIDX,:), 40, fig_rev_n);
+                title (sprintf('Population success rates, %s', stages{stageIDX}))
+                xlabel('Training stage progression'); ylabel('Success rate'), ylim([0 1])
             elseif strcmp(xfield{fieldIDX}, 'nogo')
-                plot_patch(allnogo_suc, axis_nogo, color_map(fieldIDX,:), 100, fig_rev_n);
+                plot_patch(allnogo_suc, axis_nogo, color_map(fieldIDX,:), 40, fig_rev_n);
+                xline(mean(nonzeros(insight)), '--k', 'LineWidth', 1.5)
+                text(mean(nonzeros(insight))+0.02,0.05,'Moment of insight','Rotation',90)
+                delete insight
+            elseif strcmp(xfield{fieldIDX}, 'overall')
+                plot_patch(alloverall_suc, axis_overall, stage_color_map(stageIDX,:), 40, fig_rev_n);
+                legend('Go trials','','No-Go trials','','','Overall','Location','southeast','Box','off')
             end
-            figure(stageIDX)
+
         else
             continue
         end
+        
+        % plot 'raw' success rates
+        curve1 = sucrate_mean + sucrate_std;
+        curve2 = sucrate_mean - sucrate_std;
 
         if strcmp(xfield{fieldIDX}, 'overall')
+            figure(fieldIDX)
             fill([(1:length(curve1)) fliplr((1:length(curve1)))], [curve1' fliplr(curve2')],[0 0 .85],...
                 'FaceColor',stage_color_map(stageIDX,:), 'EdgeColor','none','FaceAlpha',0.5); hold on
             plot((1:length(sucrate_mean)),sucrate_mean, 'Color', stage_color_map(stageIDX,:), 'LineWidth', 2)
 
-            xlabel('Sessions'); ylabel('Success rate')
+            xlabel('Sessions'); ylabel('Success rate'), ylim([0 1])
             title ('Population success rate')
             legend('','Initial rule','','Reversed rule','Location','southeast','Box','off')
         else
+            figure(stageIDX)
             fill([(1:length(curve1)) fliplr((1:length(curve1)))], [curve1' fliplr(curve2')],[0 0 .85],...
                 'FaceColor',color_map(fieldIDX,:), 'EdgeColor','none','FaceAlpha',0.5); hold on
             plot((1:length(sucrate_mean)),sucrate_mean, 'Color', color_map(fieldIDX,:), 'LineWidth', 2)
 
-            xlabel('Sessions'); ylabel('Success rate')
+            xlabel('Sessions'); ylabel('Success rate'), ylim([0 1])
             title (sprintf('Population success rates, %s', stages{stageIDX}))
             legend('','Go trials','','NoGo trials','Location','southeast','Box','off')
-            %ylim([0 1])
         end
     end
 
